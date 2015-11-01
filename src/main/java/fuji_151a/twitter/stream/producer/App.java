3f278ebcdeb69ac311c47350dfcb79c53e698492
yuya <life.of.151a@gmail.com>
2015-11-01 23:26:32 +0900

    first commit.

---
+++ src/main/java/fuji_151a/twitter/stream/producer/App.java
@@ -0,0 +1,13 @@
+/**
+ * App.java
+ *
+ * @since 2015/11/01
+ */
+package fuji_151a.twitter.stream.producer;
+
+/**
+ * Main Application.
+ * @author fuji-151a
+ */
+public class App {
+}
---
+++ src/test/java/fuji_151a/twitter/stream/producer/AppTest.java
@@ -0,0 +1,35 @@
+/**
+ * AppTest.java
+ *
+ * @since 2015/11/01
+ */
+package fuji_151a.twitter.stream.producer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Main Application Unit Test.
+ * @author fuji-151a
+ */
+public class AppTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void test() throws Exception {
+        // TODO Create Test Code.
+        fail();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
