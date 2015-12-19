51f4c89f6bed9407e294153ece08fe5b85b89013
yuya <life.of.151a@gmail.com>
2015-12-20 01:29:59 +0900

    comment

--- src/main/java/fuji_151a/twitter/stream/producer/App.java
+++ src/main/java/fuji_151a/twitter/stream/producer/App.java
@@ -17,21 +17,21 @@ import twitter4j.auth.AccessToken;
 public class App {
     public static void main(String[] args) {
 
-        // アクセストークン
+        // Define Access Token.
         String consumer_key = "hoge";
         String consumer_secret = "hoge";
         String access_token = "hoge";
         String secret_token = "hoge";
         AccessToken accessToken = new AccessToken(access_token, secret_token);
 
-        // Twitter Stream用のインスタンスを生成
+        // Create Twitter Stream Instance
         TwitterStreamFactory factory = new TwitterStreamFactory();
         TwitterStream twitterStream = factory.getInstance();
         twitterStream.setOAuthConsumer(consumer_key, consumer_secret);
         twitterStream.setOAuthAccessToken(accessToken);
         twitterStream.addListener(new Listener());
 
-        // Tweet 取得
+        // get tweet stream.
         twitterStream.sample();
     }
 }
