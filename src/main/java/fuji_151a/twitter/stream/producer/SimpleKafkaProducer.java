9206db5006109dd776ffaa6317655036370b7b40
yuya <life.of.151a@gmail.com>
2015-12-23 16:25:31 +0900

    modify validate

--- src/main/java/fuji_151a/twitter/stream/producer/SimpleKafkaProducer.java
+++ src/main/java/fuji_151a/twitter/stream/producer/SimpleKafkaProducer.java
@@ -8,6 +8,7 @@
  */
 package fuji_151a.twitter.stream.producer;
 
+import com.google.common.base.Preconditions;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
@@ -25,7 +26,7 @@ public class SimpleKafkaProducer {
     private KafkaProducer<String, String> producer;
     private String topic;
     private final Random rnd = new Random();
-    public static final String DEFAULT_CONF = "tweet_producer.properties";
+    public static final String DEFAULT_CONF = "producerConfig.properties";
     private static final int RANDOM_NUM = 255;
 
     public SimpleKafkaProducer() {
@@ -40,7 +41,7 @@ public class SimpleKafkaProducer {
         Properties prop = new Properties();
         try {
             prop.load(new FileInputStream(fileName));
-            propFileCheck(prop);
+            configCheck(prop);
             producer = new KafkaProducer<String, String>(prop);
             topic = prop.getProperty("topic");
         } catch (IOException e) {
@@ -49,25 +50,11 @@ public class SimpleKafkaProducer {
         }
     }
 
-    /**
-     * propertiesFile Check.
-     * @param prop : properties file
-     * @return boolean
-     */
-    private boolean propFileCheck(final Properties prop) {
-        if (prop.getProperty("topic") == null
-                && prop.getProperty("zookeeper.connect") == null
-                && prop.getProperty("metadata.broker.list") == null
-                && prop.getProperty("serializer.class") == null) {
-            System.out.println("---propertiesFile----");
-            System.out.println("zookeeper.connect=zookeeperHost:Port");
-            System.out.println("topic=TopicName");
-            System.out.println("metadata.broker.list=brokerHost:Port");
-            System.out.println("serializer.class="
-                    + "kafka.serializer.StringEncoder");
-            return false;
-        }
-        return true;
+    private void configCheck(final Properties prop) {
+        Preconditions.checkNotNull(prop.getProperty("topic"), "Not Set topic");
+        Preconditions.checkNotNull(prop.getProperty("bootstrap.servers"), "Not Set bootstrap.servers");
+        Preconditions.checkNotNull(prop.getProperty("key.serializer"), "Not Set key.serializer");
+        Preconditions.checkNotNull(prop.getProperty("value.serializer"), "Not Set value.serializer");
     }
 
     /**
@@ -79,5 +66,9 @@ public class SimpleKafkaProducer {
         ProducerRecord<String, String> data
                 = new ProducerRecord<String, String>(topic, partitionerKey, logData);
         producer.send(data);
+    }
+
+    public final void close() {
+        producer.close();
     }
 }
