614cc7c8714b0cadee733cd58fa2d25378ba7682
yuya <life.of.151a@gmail.com>
2015-12-31 01:24:40 +0900

    add DateConverter

---
+++ src/main/java/fuji/twitter/stream/consumer/DateConverter.java
@@ -0,0 +1,42 @@
+/**
+ * DateConverter.java
+ *
+ * @since 2015/12/31
+ * <p/>
+ * Copyright 2015 fuji-151a
+ * All Rights Reserved.
+ */
+package fuji.twitter.stream.consumer;
+
+import org.joda.time.DateTime;
+
+import java.util.Date;
+
+/**
+ *
+ * @author fuji-151a
+ */
+public class DateConverter {
+
+    private Date date;
+    private DateTime dt;
+
+    public DateConverter() {
+        this.date = new Date();
+        this.dt = new DateTime(date);
+    }
+
+    public DateConverter(final String d) {
+        this.date = new Date(d);
+        this.dt = new DateTime(date);
+    }
+
+    /**
+     * 指定した日付フォーマットに変換.
+     * @param date created_atの値
+     * @return yyyyMMddHHmmの形式
+     */
+    public String convertDate() {
+        return dt.toString("yyyyMMddHHmm");
+    }
+}
